d94d0a36b3114b243ab6b94cdc080c380071f139
tudor <tudorpop2003@yahoo.com>
2017-08-04 22:07:27 +0300

    Add project files.

---
+++ Problema3LAB07/Problema3LAB07.cpp
@@ -0,0 +1,68 @@
+// Problema3LAB07.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Student {
+	string nume;
+	string prenume;
+	float media;
+
+public:
+	Student( string num, string pre , float med = 0.0) 
+	{
+		media = med;
+		nume = num;
+		prenume = pre;
+	}
+	Student() {};
+	void setName(string name) { nume = name; }
+	void setPrename(string prename) { prenume = prename; }
+	void setMedia(float med) { media = med; }
+
+	float getMedia() { return media; }
+	string getName() { return nume; }
+	string getPrename() { return prenume; }
+
+	friend istream& operator>>(istream &in, Student &stud);
+	friend ostream& operator<<(ostream &out, Student &stud);
+
+};
+
+
+istream& operator>>(istream &in, Student &stud)
+{
+	cout << "Introduceti datele studentului " << endl;
+	cout << "Numele: ";
+	getline(in, stud.nume);
+	cout << "Prenume: ";
+	getline(in, stud.prenume);
+	cout << "Media: ";
+	in >> stud.media;
+	
+	return in;
+}
+
+ostream& operator<<(ostream &out, Student &stud)
+{
+	cout << "Datele studentului sunt: " << endl;
+	out << "Numele: " << stud.nume << endl;
+	out << "Prenumele: " << stud.prenume << endl;
+	out << "Media: " << stud.media << endl;
+
+	return out;
+}
+
+int main()
+{
+	Student ob1;
+	cin >> ob1;
+	cout << ob1;
+
+
+    return 0;
+}
+
